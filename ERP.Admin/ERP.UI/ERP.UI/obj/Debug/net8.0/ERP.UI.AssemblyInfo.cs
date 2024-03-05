b4dd1bf685dd5f8d0ff1d5474f84216d687f3eca
Shenith <shenithbandara@gmail.com>
2024-03-05 19:14:38 +0530

    5/3

--- ERP.Admin/ERP.UI/ERP.UI/obj/Debug/net8.0/ERP.UI.AssemblyInfo.cs
+++ ERP.Admin/ERP.UI/ERP.UI/obj/Debug/net8.0/ERP.UI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d1728cac72f9788c0c87936a9f4882a08db47dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15b7c9b1c7ac0d498446cfb00f454f33ff6bd384")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
